459ddbab0bdb9b4f921fcb3d6ed45f4b4fa0cdd2
Andrew Haupt <student@epicodus.com>
2015-10-06 15:57:52 -0700

    Add front-end form validations:

--- app/components/password-signup.js
+++ app/components/password-signup.js
@@ -14,6 +14,14 @@ export default Ember.Component.extend({
       };
       if (params.password !== params.password_confirmation) {
         Ember.get(this, 'flashMessages').danger("Password does not match password confirmation");
+      } else if (password.length < 6) {
+        Ember.get(this, 'flashMessages').danger("Password must be longer than 6 characters");
+      } else if (name.length < 1) {
+        Ember.get(this, 'flashMessages').danger("Name is required");
+      } else if (username.length < 6) {
+        Ember.get(this, 'flashMessages').danger("Username must be more than 5 characters");
+      } else if (email.length < 1){
+        Ember.get(this, 'flashMessages').danger("Email is required");
       } else {
         this.sendAction("signUp", params);
         this.set('signupShowing', false);
--- app/routes/index.js
+++ app/routes/index.js
@@ -25,6 +25,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+  model: function(){
+    return this.store.createRecord('user',{});
+  },
+
   actions: {
 
     signUp: function(params) {
