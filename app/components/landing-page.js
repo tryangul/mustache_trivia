a80bfd7f0113b07372def67acde236479ac2a42c
Andrew Haupt <ajhaupt7@gmail.com>
2015-10-06 19:03:26 -0700

    Fix logic on landing page

--- app/components/landing-page.js
+++ app/components/landing-page.js
@@ -1,8 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  model() {
-    var currentUser = this.get("session.uid")
-    return this.store.findRecord('user', currentUser)
-  }
 });
