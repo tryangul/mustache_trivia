1f2db326b24933ffdcfd34a3aefc777b06958fd8
Andrew Haupt <student@epicodus.com>
2015-10-07 11:28:51 -0700

    Remove extra code

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,19 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
-  setupController: function(controller, model){
-    this._super(controller, model);
-    this.startWatchingTime(controller);
-  },
-  startWatchingTime: function(controller){
-    var self = this;
-    controller.set('currentTime', moment());
-    Em.run.later(function(){
-      self.startWatchingTime(controller);
-    }, 1000);
-  },
-
   beforeModel: function() {
     return this.get("session").fetch().catch(function() {});
   },
--- app/routes/timer-test.js
+++ app/routes/timer-test.js
@@ -1,9 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    setupController: function(controller, model){
-      this._super(controller, model);
 
-      controller.set('anotherTime', moment());
-    }
 });
