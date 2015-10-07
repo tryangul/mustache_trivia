2007bccb621e3f3f2f6add993efa88aa6bd4dc2a
Andrew Haupt <student@epicodus.com>
2015-10-07 14:33:20 -0700

    Get ember run later to work for timer

--- app/components/timer-testing.js
+++ app/components/timer-testing.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
+
+
 export default Ember.Component.extend({
+  //  testvalue: 'hollywood',
+   //
     currentTime: function() {
       var date = new Date();
       return date.toString();
@@ -24,4 +28,32 @@ export default Ember.Component.extend({
       var roundTime = 60;
       return roundTime.toString();
     }.property('roundTimer'),
+
+    testing: function() {
+      var _this = this
+      Ember.run.later(this, function() {
+        _this.set('testvalue', 'bunker');
+      }, 10000);
+    }.observes('testvalue'),
+
+    //new stuff
+
+    init: function() {
+      // Update the time.
+      this.set('localTime', 'bunker');
+    },
+
+    // updateTime: function() {
+    //   var _this = this;
+    //
+    //   // Update the time every second.
+    //   Ember.run.later(function() {
+    //     _this.set('localTime', new Date().toLocaleTimeString());
+    //     _this.updateTime();
+    //   }, 1000);
+    // },
+
+    localTime: new Date().toLocaleTimeString()
+
+
 });
