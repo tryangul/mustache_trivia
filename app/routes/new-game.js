8147516f358074f11fe55d19b9f092fccb41061a
alyssa horrocks & ryan broughan <student@epicodus.com>
2015-10-08 12:48:44 -0700

    increment appropriate answers on game model

--- app/routes/new-game.js
+++ app/routes/new-game.js
@@ -3,24 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     var currentUserID = this.get('session.uid');
-    return Ember.RSVP.hash({
-      player1: this.store.findRecord('user', currentUserID),
-      users: this.store.findAll('user')
-  });
+    return this.store.findRecord('user', currentUserID); 
 
   },
 
   actions: {
     createGame(params) {
-      params.users = [];
-      params.users.push(params.player1);
-      params.users.push(params.player2);
       var newGame = this.store.createRecord('game', params);
 
       newGame.save().then(
         function() {
-          params.player1.save();
-          params.player2.save();
+          params.user.save();
         });
       this.transitionTo('game', newGame.id)
     }
